38e488022274d178de7bd212e771ef2a196b8008
lunyteve <timothyvowelldev@gmail.com>
2025-05-20 14:21:14 -0500

    add ability to close screen

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, color="black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
