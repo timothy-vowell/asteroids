e186a05b9e69dd730b8333d7231d24919ab69543
lunyteve <timothyvowelldev@gmail.com>
2025-05-20 16:15:32 -0500

    add player to screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,13 +9,17 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # our game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # screen.fill("black") - in the solution file
+        
         pygame.Surface.fill(screen, color="black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 fps
